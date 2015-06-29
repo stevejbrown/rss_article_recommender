12986bdde64d6a3fc67e105954f7eeaf3ca90a07
Steve Brown <stevejbrown@outlook.com>
2015-06-28 17:42:30 -0700

    Added directory structure. Added tests and test data for load_zotero.py. Added .gitignore with *.pyc ignored. Fixed faulty pandas import, column slicing in consolidate_zotero_library, and get_library reference to consolidate_zotero_library (consolidate_zotero_library -> self.consolidate_zotero_library) in load_zotero.py.

--- article_recommender/load_zotero.py
+++ article_recommender/load_zotero.py
@@ -1,4 +1,4 @@
-import pd
+import pandas as pd
 
 class ZoteroLibrary(object):
     """ Object that reads Zotero libraries and can output cleaned data to train against """
@@ -11,7 +11,7 @@ class ZoteroLibrary(object):
 
     def consolidate_zotero_library(self, library_df):
         ''' Takes a full zotero dataframe and reduces it to columns of interest '''
-        return library_df.loc['Title','Abstract Note','Automatic Tags']
+        return library_df[['Title','Abstract Note']]
 
-    def get_library(self)
-        return consolidate_zotero_library(self.library_df)
+    def get_library(self):
+        return self.consolidate_zotero_library(self.library_df)
---
+++ tests/test_load_zotero.py
@@ -0,0 +1,23 @@
+import unittest
+import os.path
+from article_recommender.load_zotero import ZoteroLibrary
+
+class TestZoteroLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.test_library = ZoteroLibrary()
+        self.test_data = 'tests/test_data/test_zotero_library.csv'
+
+    def test_read_library(self):
+        self.test_library.read_library(self.test_data)
+        self.assertIsNotNone(self.test_library.library_df)
+
+    def test_get_library(self):
+        self.test_library.read_library(self.test_data)
+        out_library = self.test_library.get_library()
+        col_names = ['Title', 'Abstract Note']
+        for name in col_names:
+            self.assertTrue(name in list(out_library))
+
+if __name__ == '__main__':
+    unittest.main()
