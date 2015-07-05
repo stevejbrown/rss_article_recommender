831a678df4bd952e5b237a95201556c7552fa723
Steve Brown <stevejbrown@outlook.com>
2015-07-04 17:13:18 -0700

    Added train_vectorizer (and unit test) to TrainingData class. Changed initialization of cleaned_library to None.

--- article_recommender/training.py
+++ article_recommender/training.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 
 class TrainingData(object):
     """ TrainingData loads an existing library of articles to train against. """
@@ -8,7 +9,13 @@ class TrainingData(object):
     def __init__(self, library):
         ''' library should be a dataframe with 'Title' and 'Abstract' columns '''
         self.library = library
-        self.cleaned_library = []
+        self.cleaned_library = None
+        self.vectorizer = CountVectorizer(analyzer = "word",
+                                          tokenizer = None,
+                                          preprocessor = None,
+                                          stop_words = None,
+                                          max_features = 1000)
+        self.weights = None
 
     def row_to_words(self, row):
         ''' Function to convert a raw entry in library entry to a string of words
@@ -39,3 +46,16 @@ class TrainingData(object):
         ''' Cleans the library with row_to_words and stores it in self.cleaned_library '''
         self.cleaned_library = self.library.apply(self.row_to_words, axis = 1)
         pass
+
+    def train_vectorizer(self):
+        ''' Trains the CountVectorizer (bag of words) model on the training data '''
+        if type(self.cleaned_library) == type(None):
+            self.clean_library()
+        train_data_features = self.vectorizer.fit_transform(self.cleaned_library)
+        train_data_features = train_data_features.toarray() # Numpy arrays are easier to work with
+        self.weights = train_data_features.sum(axis=0)
+
+        # Return a list of (word, count) touples for possible output
+        vocab = self.vectorizer.get_feature_names()
+        return [(word, count) for word, count in
+                sorted(zip(vocab, self.weights), key=lambda tup: tup[1], reverse=True)]
